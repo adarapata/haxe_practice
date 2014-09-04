2777867ffb564f9be01526cac396fb7a85f00a30
adarapata <efb.youdie@gmail.com>
2014-09-04 17:03:13 +0900

    サンプルに修正

--- TestApp/Source/Main.hx
+++ TestApp/Source/Main.hx
@@ -1,19 +1,32 @@
 package;
 
-
-import openfl.display.Sprite;
-
+import flash.display.MovieClip;
+import flash.display.Sprite;
+import flash.events.Event;
+import openfl.Assets;
+import format.SWF;
+import openfl.display.FPS;
 
 class Main extends Sprite {
-	
-	
-	public function new () {
-		
-		super ();
-		
-		
-		
-	}
-	
-	
+ 
+ 
+ var cat:MovieClip; //To load MC from swf
+ public function new () {
+    super ();
+    stage.frameRate = 24; //set frame  
+    var swf = new SWF (Assets.getBytes ("assets/walk.swf"));      
+    cat = swf.createMovieClip ("GuyWalk"); 
+    cat.height = stage.stageHeight;
+    cat.width = stage.stageWidth;
+    stage.addEventListener(Event.RESIZE, stage_onResize); 
+    addChild (cat);
+    addChild (new FPS ()); 
+ }
+
+ private function stage_onResize (event:Event):Void {
+    cat.width = stage.stageWidth;
+    cat.height = stage.stageHeight;
+ } 
+ 
+ 
 }
